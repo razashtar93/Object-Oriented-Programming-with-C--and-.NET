8a019f1681ae4e83af2eab321f169e7ef6351720
Raz Ashtar <razashtar93@gmail.com>
2022-05-04 23:14:24 +0300

    Adding EX02 to github

---
+++ B22 Ex02 Raz 307851311 Sahar 316125004/B22_Ex02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace B22_Ex02
+{
+    class Program
+    {
+        public static void Main()
+        {
+            americanCheckers();
+        }
+
+        private static void americanCheckers()
+        {
+            // Game game = new Game();
+            // playGame(); /  Game.run / or something else ..
+        }
+    }
+}
