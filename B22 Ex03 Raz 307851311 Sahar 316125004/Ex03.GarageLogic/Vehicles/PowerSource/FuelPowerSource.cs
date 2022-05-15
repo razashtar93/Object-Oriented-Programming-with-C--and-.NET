6faeb556c61e43d58d124e9e3a6d8124d9744ff8
Raz Ashtar <razashtar93@gmail.com>
2022-05-15 19:24:40 +0300

    Initialize Ex03 + adding some classes

---
+++ B22 Ex03 Raz 307851311 Sahar 316125004/Ex03.GarageLogic/Vehicles/PowerSource/FuelPowerSource.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic.Vehicles.PowerSource
+{
+    public class FuelPowerSource : PowerSource
+    {
+        private readonly eFuelType r_FuelType;
+
+        public FuelPowerSource(float i_MaxEnergy, float i_CurrentEnergy, eFuelType i_FuelType) : base(i_MaxEnergy, i_CurrentEnergy)
+        {
+            this.r_FuelType = i_FuelType;
+        }
+
+        public eFuelType FuelType
+        {
+            get { return r_FuelType; }
+        }
+
+        public void Refuel(float i_FuelLiters, eFuelType i_FuelType) 
+        {
+            float howMuchMoreFuelForMaxTank = HowMuchLeftForMaxEnergy();
+
+            if (i_FuelType != this.r_FuelType)
+            {
+                throw new ArgumentException(string.Format("Wrong type of fuel, accepting fuel: {0}", r_FuelType));
+            }
+            if (howMuchMoreFuelForMaxTank < i_FuelLiters)
+            {
+                throw new Garage.ValueOutOfRangeException(0, howMuchMoreFuelForMaxTank);
+            }
+
+            base.CurrentEnergy += i_FuelLiters;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("fuel type: {0} , Current fuel liters in the tank: {1} , max liters: {2}"
+                , r_FuelType, CurrentEnergy, MaxEnergy);
+        }
+
+        //public static Dictionary<int, eFuelType> GetFuelTypeDictionary()
+        //{
+        //    return DictUtils.GetEnumDictionary<eFuelType>();             ///TODO: do we need this?
+        //}  
+    }
+}
---
+++ B22 Ex03 Raz 307851311 Sahar 316125004/Ex03.GarageLogic/Vehicles/PowerSource/PowerSource.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic.Vehicles.PowerSource
+{
+    public abstract class PowerSource // Done. (don't forget to delete this befor submission!!!!!!)
+    {
+        private readonly float r_MaxEnergy;
+        private float m_CurrentEnergy;
+
+        public PowerSource(float i_MaxEnergy, float i_CurrentEnergy)
+        {
+            this.r_MaxEnergy = i_MaxEnergy;
+            this.m_CurrentEnergy = i_CurrentEnergy;
+        }
+
+        public float MaxEnergy
+        {
+            get { return r_MaxEnergy; }
+        }
+
+        protected float CurrentEnergy
+        {
+            set { m_CurrentEnergy = value; }
+            get { return m_CurrentEnergy; }
+        }
+
+        public float PowerEnergyPercent
+        {
+            get { return (m_CurrentEnergy / r_MaxEnergy) * 100; }
+        }
+
+        public float HowMuchLeftForMaxEnergy()
+        {
+            return (this.r_MaxEnergy - this.m_CurrentEnergy);
+
+
+        }
+    }
+}
