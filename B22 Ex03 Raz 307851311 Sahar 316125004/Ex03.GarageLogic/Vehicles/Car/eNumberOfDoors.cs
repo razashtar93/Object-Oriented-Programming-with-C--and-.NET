f08b2f8e63c47a03aa14ee7655b732428e26bacc
Raz Ashtar <razashtar93@gmail.com>
2022-05-15 19:43:57 +0300

    Adding more classes

---
+++ B22 Ex03 Raz 307851311 Sahar 316125004/Ex03.GarageLogic/Vehicles/Car/eNumberOfDoors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic.Vehicles.Car
+{
+    public enum eNumberOfDoors
+    {
+        Two,
+        Three,
+        Four,
+        Five
+    }
+}
--- B22 Ex03 Raz 307851311 Sahar 316125004/Ex03.GarageLogic/Vehicles/PowerSource/ElectricPowerSource.cs
+++ B22 Ex03 Raz 307851311 Sahar 316125004/Ex03.GarageLogic/Vehicles/PowerSource/ElectricPowerSource.cs
@@ -5,7 +5,29 @@ using System.Text;
 
 namespace Ex03.GarageLogic.Vehicles.PowerSource
 {
-    class ElectricPowerSource
+    public class ElectricPowerSource : PowerSource
     {
+        public ElectricPowerSource(float i_MaxEnergy, float i_CurrentEnergy) : base(i_MaxEnergy, i_CurrentEnergy)
+        {
+            
+        }
+
+        public void Recharge(float i_Hours)
+        {
+            float howMuchMoreToRecharge = HowMuchLeftForMaxEnergy();
+
+            if (howMuchMoreToRecharge < i_Hours)
+            {
+                throw new Garage.ValueOutOfRangeException(0, howMuchMoreToRecharge);
+            }
+
+            base.CurrentEnergy += i_Hours;
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("Battery current hours: {0} , max hours: {1}", CurrentEnergy, MaxEnergy);
+        }
     }
 }
