8370538a11b27d48d3852c3cd067d3f8982db85d
Raz Ashtar <razashtar93@gmail.com>
2022-05-16 16:09:32 +0300

    working on the Vehicle Generator and the Vehicle class

--- B22 Ex03 Raz 307851311 Sahar 316125004/Ex03.GarageLogic/Garage/VehicleGenerator.cs
+++ B22 Ex03 Raz 307851311 Sahar 316125004/Ex03.GarageLogic/Garage/VehicleGenerator.cs
@@ -2,42 +2,179 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Ex03.GarageLogic.Vehicles;
+using Ex03.GarageLogic.Vehicles.Car;
+using Ex03.GarageLogic.Vehicles.Motorcycle;
+using Ex03.GarageLogic.Vehicles.PowerSource;
+using Ex03.GarageLogic.Vehicles.Truck;
 
 namespace Ex03.GarageLogic.Garage
 {
-    class VehicleGenerator
+    public class VehicleGenerator
     {
-        private const int k_NumberOfWheelsMotorcycleFuel = 2;
-        private const float k_MaxAirPressureInWheelsMotorcycleFuel = 31;
-        private const Vehicles.PowerSource.eFuelType k_FuelTypeMotorcycleFuel = Vehicles.PowerSource.eFuelType.Octan98;
-        private const float k_TankFuelMotorcycleFuel = 6.2f;
+        private const int k_FuelMotorcycleNumberOfWheels = 2;
+        private const float k_FuelMotorcycleMaxAirPressureInWheels = 31;
+        private const Vehicles.PowerSource.eFuelType k_FuelMotorcycleFuelType = Vehicles.PowerSource.eFuelType.Octan98;
+        private const float k_FuelMotorcycleTankFuel = 6.2f;
 
-        private const int k_NumberOfWheelsMotorcycleElectric = 2;
-        private const float k_MaxAirPressureInWheelsMotorcycleElectric = 31;
-        private const float k_MaximumBatteryLifeMotorcycleElectric = 2.5f;
+        private const int k_ElectricMotorcycleNumberOfWheels = 2;
+        private const float k_ElectricMotorcycleMaxAirPressureInWheels = 31;
+        private const float k_ElectricMotorcycleMaximumBatteryLife = 2.5f;
 
-        private const int k_NumberOfWheelsCarFuel = 4;
-        private const float k_MaxAirPressureInWheelsCarFuel = 29;
-        private const Vehicles.PowerSource.eFuelType k_FuelTypeCarFuel = Vehicles.PowerSource.eFuelType.Octan95;
-        private const float k_TankFuelCarFuel = 38f;
-
-
-        private const int k_NumberOfWheelsCarElectric = 4;
-        private const float k_MaxAirPressureInWheelsCarElectric = 29;
-        private const float k_MaximumBatteryLifeCarElectric = 3.3f;
-
-        private const int k_NumberOfWheelsTruckFuel= 16;
-        private const float k_MaxAirPressureInWheelsTruckFuel = 24;
-        private const Vehicles.PowerSource.eFuelType k_FuelTypeTruckFuel = Vehicles.PowerSource.eFuelType.Soler;
-        private const float k_TankFuelTruckFuel = 120f;
+        private const int k_FualCarNumberOfWheels = 4;
+        private const float k_FualCarMaxAirPressureInWheels = 29;
+        private const Vehicles.PowerSource.eFuelType k_FualCarFuelType = Vehicles.PowerSource.eFuelType.Octan95;
+        private const float k_FualCarTankFuel = 38f;
 
 
+        private const int k_ElectricCarNumberOfWheels = 4;
+        private const float k_ElectricCarMaxAirPressureInWheels = 29;
+        private const float k_ElectricCarMaximumBatteryLife = 3.3f;
+
+        private const int k_TruckNumberOfWheels = 16;
+        private const float k_TruckMaxAirPressureInWheels = 24;
+        private const Vehicles.PowerSource.eFuelType k_TruckFuelType = Vehicles.PowerSource.eFuelType.Soler;
+        private const float k_TruckTankFuel = 120f;
+
+
+        public static Vehicles.Vehicle CreateVehicle(string i_ModelName, string i_LicencePlate, eVehicleType i_VehicleType, string i_WheelManufacturerName, float i_WheelCurrentAirPressure, float i_CurrentEnergy)
+        {
+            Vehicle newVehicle = null;
+            PowerSource powerSource;
+            Wheel[] wheels;
+
+            switch (i_VehicleType)
+            {
+                case eVehicleType.FuelMotorcycle:
+                    wheels = createWheels(k_FuelMotorcycleNumberOfWheels, i_WheelManufacturerName,
+                        i_WheelCurrentAirPressure, k_FuelMotorcycleMaxAirPressureInWheels);
+
+                    powerSource = new FuelPowerSource(k_FuelMotorcycleTankFuel, i_CurrentEnergy, k_FuelMotorcycleFuelType);
+
+                    newVehicle = new Motorcycle(i_ModelName, i_LicencePlate, wheels, powerSource, i_VehicleType);
+                    break;
+
+                case eVehicleType.ElectricMotorcycle:
+                    wheels = createWheels(k_ElectricMotorcycleNumberOfWheels, i_WheelManufacturerName,
+                        i_WheelCurrentAirPressure, k_ElectricMotorcycleMaxAirPressureInWheels);
+
+                    powerSource = new ElectricPowerSource(k_ElectricMotorcycleMaximumBatteryLife, i_CurrentEnergy);
+
+                    newVehicle = new Motorcycle(i_ModelName, i_LicencePlate, wheels, powerSource, i_VehicleType);
+                    break;
+
+                case eVehicleType.FualCar:
+                    wheels = createWheels(k_FualCarNumberOfWheels, i_WheelManufacturerName,
+                        i_WheelCurrentAirPressure, k_FualCarMaxAirPressureInWheels);
+
+                    powerSource = new FuelPowerSource(k_FualCarTankFuel, i_CurrentEnergy, k_FualCarFuelType);
+
+                    newVehicle = new Car(i_ModelName, i_LicencePlate, wheels, powerSource, i_VehicleType);
+                    break;
+
+                case eVehicleType.ElectricCar:
+                    wheels = createWheels(k_ElectricCarNumberOfWheels, i_WheelManufacturerName,
+                        i_WheelCurrentAirPressure, k_ElectricCarMaxAirPressureInWheels);
+
+                    powerSource = new ElectricPowerSource(k_ElectricCarMaximumBatteryLife, i_CurrentEnergy);
+
+                    newVehicle = new Car(i_ModelName, i_LicencePlate, wheels, powerSource, i_VehicleType);
+                    break;
+
+                case eVehicleType.Truck:
+                    wheels = createWheels(k_TruckNumberOfWheels, i_WheelManufacturerName,
+                       i_WheelCurrentAirPressure, k_TruckMaxAirPressureInWheels);
+
+                    powerSource = new FuelPowerSource(k_TruckTankFuel, i_CurrentEnergy, k_TruckFuelType);
+
+                    newVehicle = new Truck(i_ModelName, i_LicencePlate, wheels, powerSource, i_VehicleType);
+                    break;
+            }
+
+            return newVehicle;
+        }
 
 
 
+        private static Wheel[] createWheels(int i_NumberOfWheels, string i_WheelManufacturerName, float i_WheelCurrentAirPressure, float i_MaxAirPressure)
+        {
+            Wheel[] wheels = new Wheel[i_NumberOfWheels];
+
+            for (int i = 0; i < i_NumberOfWheels; i++)
+            {
+                wheels[i] = new Wheel(i_WheelManufacturerName, i_WheelCurrentAirPressure, i_MaxAirPressure);
+            }
+
+            return wheels;
+
+        }
 
 
-        public enum eVehicleType
+        public static float GetVehicleMaxEnergy(eVehicleType i_VehicleType)
+        {
+            float maxEnergy = 0;
+
+            switch (i_VehicleType)
+            {
+                case eVehicleType.FuelMotorcycle:
+                    maxEnergy = k_FuelMotorcycleTankFuel;
+                    break;
+
+                case eVehicleType.ElectricMotorcycle:
+                    maxEnergy = k_ElectricMotorcycleMaximumBatteryLife;
+                    break;
+
+                case eVehicleType.FualCar:
+                    maxEnergy = k_FualCarTankFuel;
+                    break;
+
+                case eVehicleType.ElectricCar:
+                    maxEnergy = k_ElectricCarMaximumBatteryLife;
+                    break;
+
+                case eVehicleType.Truck:
+                    maxEnergy = k_TruckTankFuel;
+                    break;
+            }
+
+            return maxEnergy;
+        }
+
+
+        public static float GetVehicleMaxAirPressurre(eVehicleType i_VehicleType)
+        {
+            float maxAir = 0;
+
+            switch (i_VehicleType)
+            {
+                case eVehicleType.FuelMotorcycle:
+                    maxAir = k_FuelMotorcycleMaxAirPressureInWheels;
+                    break;
+
+                case eVehicleType.ElectricMotorcycle:
+                    maxAir = k_ElectricMotorcycleMaxAirPressureInWheels;
+                    break;
+
+                case eVehicleType.FualCar:
+                    maxAir = k_FualCarMaxAirPressureInWheels;
+                    break;
+
+
+
+                case eVehicleType.ElectricCar:
+                    maxAir = k_ElectricCarMaxAirPressureInWheels;
+                    break;
+
+                case eVehicleType.Truck:
+                    maxAir = k_TruckMaxAirPressureInWheels;
+                    break;
+            }
+
+            return maxAir;
+        }
+
+
+        public enum eVehicleType // when we want to add another type of vehicle we add the type here
         {
             FuelMotorcycle,
             ElectricMotorcycle,
--- B22 Ex03 Raz 307851311 Sahar 316125004/Ex03.GarageLogic/Vehicles/Motorcycle/Motorcycle.cs
+++ B22 Ex03 Raz 307851311 Sahar 316125004/Ex03.GarageLogic/Vehicles/Motorcycle/Motorcycle.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Ex03.GarageLogic.Garage;
 
 namespace Ex03.GarageLogic.Vehicles.Motorcycle
 {
     public class Motorcycle : Vehicle
     {
+        private eLicenceType m_LicenceType;
+        private int m_EngineCapacity;
+
+
+        public Motorcycle(string i_ModelName, string i_LicencePlate, Wheel[] i_Wheel, PowerSource i_PowerSource, VehicleGenerator.eVehicleType i_VehicleType)
+            : base(i_ModelName, i_LicencePlate, i_Wheel, i_PowerSource, i_VehicleType)
+        {
+
+        }
+
+        public Motorcycle(string i_ModelName, string i_LicencePlate, Wheel[] i_Wheel, PowerSource i_PowerSource,
+            eLicenceType i_LicenceType, int i_EngineCapacity, Garage.VehicleGenerator.eVehicleType i_VehicleType)
+            : base(i_ModelName, i_LicencePlate, i_Wheel, i_PowerSource, i_VehicleType)
+        {
+            m_LicenceType = i_LicenceType;
+            m_EngineCapacity = i_EngineCapacity;
+        }
     }
 }
