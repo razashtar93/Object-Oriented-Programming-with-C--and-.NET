533aeca472383d33cebbc91fa379c7bcdfbafe6a
Raz Ashtar <razashtar93@gmail.com>
2022-05-17 15:53:19 +0300

    update some classes

--- B22 Ex03 Raz 307851311 Sahar 316125004/Ex03.GarageLogic/Garage/Owner.cs
+++ B22 Ex03 Raz 307851311 Sahar 316125004/Ex03.GarageLogic/Garage/Owner.cs
@@ -7,5 +7,20 @@ namespace Ex03.GarageLogic
 {
     public class Owner
     {
+        private string m_Name;
+        private string m_PhoneNumber;
+
+        public Owner(string i_Name, string i_PhoneNumber)
+        {
+            m_Name = i_Name;
+            m_PhoneNumber = i_PhoneNumber;
+        }
+
+
+
+        public override string ToString()
+        {
+            return String.Format("Owner: Name - {0} , Phone Number - {1}", m_Name, m_PhoneNumber);
+        }
     }
 }
