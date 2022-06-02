484eca2d3358d94628b175d7eb218b0b581989e6
Raz Ashtar <razashtar93@gmail.com>
2022-06-02 21:38:27 +0300

    almost done GameSetupForm

--- B22 Ex05 Raz 307851311 Sahar 316125004/B22_Ex05/GameSetupForm.Designer.cs
+++ B22 Ex05 Raz 307851311 Sahar 316125004/B22_Ex05/GameSetupForm.Designer.cs
@@ -29,55 +29,55 @@ namespace B22_Ex05
         /// </summary>
         private void InitializeComponent()
         {
-            this.BoardSize = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.LabelBoardSize = new System.Windows.Forms.Label();
+            this.textBoxPlayer1 = new System.Windows.Forms.TextBox();
+            this.LabelPlayers = new System.Windows.Forms.Label();
+            this.LabelPlayer1 = new System.Windows.Forms.Label();
             this.radioButton6x6 = new System.Windows.Forms.RadioButton();
             this.radioButton8x8 = new System.Windows.Forms.RadioButton();
             this.radioButton10x10 = new System.Windows.Forms.RadioButton();
             this.ButtonDone = new System.Windows.Forms.Button();
-            this.Player2 = new System.Windows.Forms.CheckBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.CheckBoxPlayer2 = new System.Windows.Forms.CheckBox();
+            this.textBoxPlayer2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // BoardSize
+            // LabelBoardSize
             // 
-            this.BoardSize.AutoSize = true;
-            this.BoardSize.Location = new System.Drawing.Point(21, 22);
-            this.BoardSize.Name = "BoardSize";
-            this.BoardSize.Size = new System.Drawing.Size(91, 20);
-            this.BoardSize.TabIndex = 0;
-            this.BoardSize.Text = "Board Size:";
-            this.BoardSize.AutoSizeChanged += new System.EventHandler(this.GameSetupForm_Load);
-            this.BoardSize.Click += new System.EventHandler(this.label1_Click);
+            this.LabelBoardSize.AutoSize = true;
+            this.LabelBoardSize.Location = new System.Drawing.Point(21, 22);
+            this.LabelBoardSize.Name = "LabelBoardSize";
+            this.LabelBoardSize.Size = new System.Drawing.Size(91, 20);
+            this.LabelBoardSize.TabIndex = 0;
+            this.LabelBoardSize.Text = "Board Size:";
+            this.LabelBoardSize.AutoSizeChanged += new System.EventHandler(this.GameSetupForm_Load);
+            this.LabelBoardSize.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox1
+            // textBoxPlayer1
             // 
-            this.textBox1.Location = new System.Drawing.Point(149, 140);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(170, 26);
-            this.textBox1.TabIndex = 1;
+            this.textBoxPlayer1.Location = new System.Drawing.Point(149, 140);
+            this.textBoxPlayer1.Name = "textBoxPlayer1";
+            this.textBoxPlayer1.Size = new System.Drawing.Size(170, 26);
+            this.textBoxPlayer1.TabIndex = 6;
             // 
-            // label1
+            // LabelPlayers
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 107);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 20);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Players:";
+            this.LabelPlayers.AutoSize = true;
+            this.LabelPlayers.Location = new System.Drawing.Point(21, 107);
+            this.LabelPlayers.Name = "LabelPlayers";
+            this.LabelPlayers.Size = new System.Drawing.Size(64, 20);
+            this.LabelPlayers.TabIndex = 4;
+            this.LabelPlayers.Text = "Players:";
             // 
-            // label2
+            // LabelPlayer1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(37, 146);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Player 1:";
-            this.label2.Visible = false;
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.LabelPlayer1.AutoSize = true;
+            this.LabelPlayer1.Location = new System.Drawing.Point(37, 146);
+            this.LabelPlayer1.Name = "LabelPlayer1";
+            this.LabelPlayer1.Size = new System.Drawing.Size(69, 20);
+            this.LabelPlayer1.TabIndex = 5;
+            this.LabelPlayer1.Text = "Player 1:";
+            this.LabelPlayer1.Visible = false;
+            this.LabelPlayer1.Click += new System.EventHandler(this.label2_Click);
             // 
             // radioButton6x6
             // 
@@ -87,9 +87,11 @@ namespace B22_Ex05
             this.radioButton6x6.Location = new System.Drawing.Point(41, 54);
             this.radioButton6x6.Name = "radioButton6x6";
             this.radioButton6x6.Size = new System.Drawing.Size(67, 24);
-            this.radioButton6x6.TabIndex = 4;
+            this.radioButton6x6.TabIndex = 1;
+            this.radioButton6x6.TabStop = true;
             this.radioButton6x6.Text = "6 x 6";
             this.radioButton6x6.UseVisualStyleBackColor = true;
+            this.radioButton6x6.CheckedChanged += new System.EventHandler(this.radioButton6x6_CheckedChanged);
             // 
             // radioButton8x8
             // 
@@ -98,9 +100,10 @@ namespace B22_Ex05
             this.radioButton8x8.Location = new System.Drawing.Point(124, 54);
             this.radioButton8x8.Name = "radioButton8x8";
             this.radioButton8x8.Size = new System.Drawing.Size(67, 24);
-            this.radioButton8x8.TabIndex = 7;
+            this.radioButton8x8.TabIndex = 2;
             this.radioButton8x8.Text = "8 x 8";
             this.radioButton8x8.UseVisualStyleBackColor = true;
+            this.radioButton8x8.CheckedChanged += new System.EventHandler(this.radioButton8x8_CheckedChanged);
             // 
             // radioButton10x10
             // 
@@ -109,7 +112,7 @@ namespace B22_Ex05
             this.radioButton10x10.Location = new System.Drawing.Point(209, 54);
             this.radioButton10x10.Name = "radioButton10x10";
             this.radioButton10x10.Size = new System.Drawing.Size(85, 24);
-            this.radioButton10x10.TabIndex = 8;
+            this.radioButton10x10.TabIndex = 3;
             this.radioButton10x10.Text = "10 x 10";
             this.radioButton10x10.UseVisualStyleBackColor = true;
             this.radioButton10x10.CheckedChanged += new System.EventHandler(this.radioButton10x10_CheckedChanged);
@@ -125,27 +128,28 @@ namespace B22_Ex05
             this.ButtonDone.TabIndex = 9;
             this.ButtonDone.Text = "Done";
             this.ButtonDone.UseVisualStyleBackColor = true;
+            this.ButtonDone.Click += new System.EventHandler(this.ButtonDone_Click);
             // 
-            // Player2
+            // CheckBoxPlayer2
             // 
-            this.Player2.AutoSize = true;
-            this.Player2.Location = new System.Drawing.Point(41, 188);
-            this.Player2.Name = "Player2";
-            this.Player2.Size = new System.Drawing.Size(95, 24);
-            this.Player2.TabIndex = 10;
-            this.Player2.Text = "Player 2:";
-            this.Player2.UseVisualStyleBackColor = true;
-            this.Player2.CheckedChanged += new System.EventHandler(this.Player2_CheckedChanged);
+            this.CheckBoxPlayer2.AutoSize = true;
+            this.CheckBoxPlayer2.Location = new System.Drawing.Point(41, 188);
+            this.CheckBoxPlayer2.Name = "CheckBoxPlayer2";
+            this.CheckBoxPlayer2.Size = new System.Drawing.Size(95, 24);
+            this.CheckBoxPlayer2.TabIndex = 7;
+            this.CheckBoxPlayer2.Text = "Player 2:";
+            this.CheckBoxPlayer2.UseVisualStyleBackColor = true;
+            this.CheckBoxPlayer2.CheckedChanged += new System.EventHandler(this.Player2_CheckedChanged);
             // 
-            // textBox2
+            // textBoxPlayer2
             // 
-            this.textBox2.Location = new System.Drawing.Point(149, 188);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(170, 26);
-            this.textBox2.TabIndex = 11;
-            this.textBox2.Text = "[Computer]";
-            this.textBox2.Visible = false;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBoxPlayer2.Location = new System.Drawing.Point(149, 188);
+            this.textBoxPlayer2.Name = "textBoxPlayer2";
+            this.textBoxPlayer2.Size = new System.Drawing.Size(170, 26);
+            this.textBoxPlayer2.TabIndex = 8;
+            this.textBoxPlayer2.Text = "[Computer]";
+            this.textBoxPlayer2.Visible = false;
+            this.textBoxPlayer2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // GameSetupForm
             // 
@@ -153,18 +157,17 @@ namespace B22_Ex05
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(354, 282);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.Player2);
+            this.ClientSize = new System.Drawing.Size(355, 290);
+            this.Controls.Add(this.textBoxPlayer2);
+            this.Controls.Add(this.CheckBoxPlayer2);
             this.Controls.Add(this.ButtonDone);
             this.Controls.Add(this.radioButton10x10);
             this.Controls.Add(this.radioButton8x8);
             this.Controls.Add(this.radioButton6x6);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.BoardSize);
-            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Controls.Add(this.LabelPlayer1);
+            this.Controls.Add(this.LabelPlayers);
+            this.Controls.Add(this.textBoxPlayer1);
+            this.Controls.Add(this.LabelBoardSize);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "GameSetupForm";
@@ -179,15 +182,15 @@ namespace B22_Ex05
 
         #endregion
 
-        private System.Windows.Forms.Label BoardSize;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LabelBoardSize;
+        private System.Windows.Forms.TextBox textBoxPlayer1;
+        private System.Windows.Forms.Label LabelPlayers;
+        private System.Windows.Forms.Label LabelPlayer1;
         private System.Windows.Forms.RadioButton radioButton6x6;
         private System.Windows.Forms.RadioButton radioButton8x8;
         private System.Windows.Forms.RadioButton radioButton10x10;
         private System.Windows.Forms.Button ButtonDone;
-        private System.Windows.Forms.CheckBox Player2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox CheckBoxPlayer2;
+        private System.Windows.Forms.TextBox textBoxPlayer2;
     }
 }
